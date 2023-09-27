51d009b9f00f39e4758278c60a0369a9c4ad6d2f
Nilesh Viduranga <nnviduranga@gmail.com>
2023-09-28 01:16:43 +0530

    Added user characters page

--- GenshinAIOApp/navigation/Tabs.tsx
+++ GenshinAIOApp/navigation/Tabs.tsx
@@ -1,8 +1,13 @@
 import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 import Icon from 'react-native-vector-icons/FontAwesome';
+import {Image} from 'react-native';
 
 import StackNavigator from './Stack';
 import Profile from '../screens/Profile';
+import UserCharacters from '../screens/UserCharacters';
+
+import enkaLogo from '../assets/images/enka.png';
+import characterLogo from '../assets/images/characters.png';
 
 export default function Tabs() {
   const Tab = createBottomTabNavigator();
@@ -21,9 +26,44 @@ export default function Tabs() {
         name="Characters"
         component={StackNavigator}
         options={{
-          tabBarIcon: ({size, focused}) => (
-            <Icon name="shield" size={size} color={focused ? '#fff' : 'gray'} />
-          ),
+          tabBarIcon: ({size, focused}) => {
+            return (
+              <Image
+                style={{
+                  width: size,
+                  height: size,
+                  tintColor: focused ? '#fff' : 'gray',
+                }}
+                source={characterLogo}
+              />
+            );
+          },
+          headerShown: false,
+          tabBarStyle: tabBarStyle,
+          tabBarLabelStyle: {
+            fontSize: 12,
+            fontWeight: 'bold',
+            color: '#fff',
+          },
+        }}
+      />
+      {/* user characters */}
+      <Tab.Screen
+        name="Enka Network"
+        component={UserCharacters}
+        options={{
+          tabBarIcon: ({size, focused}) => {
+            return (
+              <Image
+                style={{
+                  width: size,
+                  height: size,
+                  tintColor: focused ? '#fff' : 'gray',
+                }}
+                source={enkaLogo}
+              />
+            );
+          },
           headerShown: false,
           tabBarStyle: tabBarStyle,
           tabBarLabelStyle: {
---
+++ GenshinAIOApp/screens/UserCharacters.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import {Text, View} from 'react-native';
+
+type Props = {};
+
+export default function UserCharacters() {
+  return (
+    <View className="bg-black w-full h-full p-4">
+      <Text>UserCharacters</Text>
+    </View>
+  );
+}
